f32238ff274ceaca2e070a0bd3293e2bd8275872
Oleksandr Vashchuk <112938251+MisterF1x@users.noreply.github.com>
2023-02-24 19:21:31 +0200

    set cors mode

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,7 +2,7 @@ const url = 'https://restcountries.com/v3.1';
 const fields = 'name,capital,population,flags,languages';
 
 export const fetchCountries = name => {
-  return fetch(`${url}/name/${name}/?fields=${fields}`).then(response => {
+  return fetch(`${url}/name/${name}/?fields=${fields}`, {mode: 'no-cors'}).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
